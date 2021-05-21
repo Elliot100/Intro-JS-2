33cc43a1e3e3973bafaf33239e772b0450eb804c
Elly <ellychau10@yahoo.com>
2021-05-21 12:11:59 -0700

    Final Commit

--- elephant.js
+++ elephant.js
@@ -37,3 +37,13 @@ Elephant.paradeHelper = function (elephant) {
 // let herd = [ellie, charlie, kate, micah];
 
 // herd.forEach(Elephant.paradeHelper);
+
+function dinerBreakfast() {
+  let order = "I'd like cheesy scrambled eggs please";
+  console.log(order);
+
+  return function (food) {
+    order = `${order.slice(0, order.length - 8)} and ${food} please.`;
+    console.log(order);
+  };
+}
